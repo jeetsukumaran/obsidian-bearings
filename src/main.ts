092b96f78c405f526e245579dff2d8d9f385d14c
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-04-10 20:33:13 -0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ export default class BearingsPlugin extends Plugin {
 
         /* Code block */
         this.registerMarkdownCodeBlockProcessor(
-            "bearings",
+            "bearings-view",
             (source, el, ctx) => {
                 // source = source.trim();
                 // let trimmedSource = source.trim()
