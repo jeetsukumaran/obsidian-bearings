3fd6d2a9bddb77e1c4a1bd610ab7a0ffb3e70765
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-04-18 16:57:49 -0700

    Update

--- src/menus.ts
+++ src/menus.ts
@@ -1,31 +1,10 @@
 import {
-    // ItemView,
     App,
-    // Editor,
-    // TFile,
-    // MarkdownView,
     PaneType,
     Menu,
-    // Modal,
     Notice,
-    // Plugin,
-    // PluginSettingTab,
-    // Setting,
-    // WorkspaceLeaf,
-    // setIcon,
-    // CachedMetadata,
     normalizePath,
-    // FileSystemAdapter,
 } from "obsidian";
-
-// function getVaultBasePath(): string {
-//     const adapter = app.vault.adapter;
-//     if (adapter instanceof FileSystemAdapter) {
-//         return adapter.getBasePath();
-//     }
-//     return "";
-// }
-
 
 export function buildLinkOpenMenu(
     menu: Menu,
@@ -90,117 +69,6 @@ export function buildLinkOpenMenu(
             )
     );
 }
-
-async function createUniqueNote(
-    app: App,
-    directoryPath: string,
-    frontmatter: string,
-    mode: PaneType | undefined,
-): Promise<string> {
-
-    const path = require('path');
-    let counter = 0;
-    let newNotePath;
-
-    do {
-        const fileName = `Untitled${counter ? ` ${counter}` : ''}.md`;
-        newNotePath = [directoryPath, fileName].join("/");
-        counter++;
-    } while (await app.vault.adapter.exists(newNotePath));
-
-    try {
-        await app.vault.create(newNotePath, frontmatter);
-        app.workspace.openLinkText(newNotePath, '', mode);
-    } catch (error) {
-        console.error('Error creating or opening the new note:', error);
-    }
-    return newNotePath
-}
-
-
-export function buildLinkCreateMenu(
-    menu: Menu,
-    app: App,
-    linkPath: string,
-    getCloneEntryRelationships?: () => Map<string, string[]>,
-    includePreSeparator = true,
-) {
-    const path = require('path');
-    const fs = require('fs');
-
-    // Function to create a new note with specific content in the directory
-    async function createAndOpenNoteInDirectory(mode: PaneType | undefined) {
-        const directoryPath = path.dirname(linkPath);
-
-        // Check if linkPath is a file, then use its parent
-        // if (fs.existsSync(linkPath) && !fs.statSync(linkPath).isDirectory()) {
-        //     directoryPath = path.dirname(linkPath);
-        // }
-
-        // Prevent going beyond the root of the vault
-        // const rootPath = app.vault.getRoot().path;
-        // if (!directoryPath.startsWith(rootPath)) {
-        //     directoryPath = rootPath;
-        // }
-
-        // Generate the current date and time in the desired format
-        const currentDateTime = new Date()
-        const formattedDateTime = currentDateTime.toISOString().replace(/:\d{2}\.\d{3}Z$/, '');
-
-        // Create the YAML frontmatter content
-        const frontmatterParts = []
-        frontmatterParts.push("---")
-        frontmatterParts.push(`entry-date: ${formattedDateTime}`)
-        // frontmatterParts.push(`entry-parents:`)
-        // frontmatterParts.push(`  - "[[${linkPath}]]"`)
-        if (getCloneEntryRelationships) {
-            getCloneEntryRelationships().forEach( (subEntries: string[], propertyName: string) => {
-                frontmatterParts.push(`${propertyName}:`)
-                subEntries.forEach( (subEntryPath) => frontmatterParts.push(`  - "[[${subEntryPath}]]"`) )
-            })
-        }
-        frontmatterParts.push("---")
-        const frontmatter = frontmatterParts.join("\n")
-
-        // const newNotePath = path.join(directoryPath, `Untitled.md`);
-        // try {
-        //     await app.vault.create(newNotePath, frontmatter); // Create a note with the frontmatter
-        //     app.workspace.openLinkText(newNotePath, '', mode); // Open the new note
-        // } catch (error) {
-        //     console.error('Error creating or opening the new note:', error);
-        // }
-        createUniqueNote(
-            app,
-            directoryPath,
-            frontmatter,
-            mode,
-        )
-
-    }
-
-    if (includePreSeparator) {
-        menu.addSeparator();
-    }
-
-    // Add options to create and open a new note
-    menu.addItem((item) =>
-        item
-            .setTitle("Create sibling note")
-            .setIcon("document")
-            .onClick(() => createAndOpenNoteInDirectory(undefined))
-    );
-
-    menu.addItem((item) =>
-        item
-            .setTitle("Create sibling note in new tab")
-            .setIcon("document")
-            .onClick(() => createAndOpenNoteInDirectory('tab'))
-    );
-
-}
-
-
-
 
 function _copyToClipboard(text: string) {
     navigator.clipboard.writeText(text).then(() => {
