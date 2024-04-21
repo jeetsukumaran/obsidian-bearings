031b5e0a0b5c8205773ab80d330ed6c1ceff5e2e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-04-21 16:09:15 -0700

    Update

--- src/view.ts
+++ src/view.ts
@@ -1053,8 +1053,8 @@ export class NavigationEntryFrame extends NavigationBase {
         // }
 
         this.elements.entryHeadContent = this.elements.entryHead.createEl("div", { cls: "bearings-entry-head-content"});
-        this.elements.entryHeadLinkContainer = this.elements.entryHeadContent.createEl("div", { cls: "bearings-entry-head-link-container"});
         this.elements.entryGlyphRow = this.elements.entryHeadContent.createEl("div", { cls: "bearings-entry-head-icon-row"});
+        this.elements.entryHeadLinkContainer = this.elements.entryHeadContent.createEl("div", { cls: "bearings-entry-head-link-container"});
 
         this.renderEntryLink(
             this.elements.entryHeadLinkContainer,
