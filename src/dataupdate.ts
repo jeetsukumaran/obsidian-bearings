f44f1626cc6decf3dc557b20d1657f5fc81048f8
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-06-05 22:51:05 -0700

    Update

--- src/dataupdate.ts
+++ src/dataupdate.ts
@@ -124,10 +124,11 @@ export class CreateRelationshipModal extends Modal {
     async selectionUpdate() {
         let currentSelection = this.currentSelection;
         // this.relationshipDescEl.setText(`[[${this.focalFilePath}]] will designate [[${this.linkPath}]] as: ${currentSelection.designatedRelationshipLabel}`);
-        this.designatedRelationshipLabelEl.setText(currentSelection.designatedRelationshipLabel);
-        this.designatedRelationshipValueEl.setText(this.linkPath);
         this.invertedRelationshipLabelEl.setText(currentSelection.invertedRelationshipLabel);
-        this.invertedRelationshipValueEl.setText(this.focalFilePath);
+        this.invertedRelationshipValueEl.setText(`[[${this.focalFilePath}]]: '${this.focalFilePathDisplayTitle}'`);
+        this.designatedRelationshipLabelEl.setText(currentSelection.designatedRelationshipLabel);
+        // this.designatedRelationshipValueEl.setText(this.linkPath);
+        this.designatedRelationshipValueEl.setText(`[[${this.linkPath}]]: '${this.linkPathDisplayTitle}'`);
         this.headerEl.setText(`Add ${currentSelection.designatedRelationshipLabel || 'relationship link'}`);
     }
 
